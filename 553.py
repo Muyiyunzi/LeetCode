a9abb040b5f0192f45d7e112f4773181f5e77576
muyiyunzi <gt3115a@163.com>
2022-02-27 20:12:22 +0800

    553. 最优除法

--- 553.py
+++ 553.py
@@ -21,3 +21,40 @@ class Solution:
         if len(nums) == 2:
             return str(nums[0]) + '/' + str(nums[1])
         return str(nums[0]) + '/(' + "/".join(map(str, nums[1:])) + ')'
+
+# 第三版 DP
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+        n = len(nums)
+        dptable = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(2)]
+        dpstr = [[['' for _ in range(n)] for _ in range(n)] for _ in range(2)]
+        for i in range(n):
+            dptable[0][i][i] = nums[i]
+            dptable[1][i][i] = nums[i]
+            dpstr[0][i][i] = str(nums[i])
+            dpstr[1][i][i] = str(nums[i])
+        for i in range(1, n):
+            for j in range(n-i):
+                Min = inf
+                Max = 0
+                index_min, index_max = 0, 0
+                for k in range(i):
+                    now_min = dptable[0][j][j+k] / dptable[1][j+k+1][j+i]
+                    now_max = dptable[1][j][j+k] / dptable[0][j+k+1][j+i]
+                    if now_min < Min:
+                        Min = now_min
+                        index_min = k                        
+                    if now_max > Max:
+                        Max = now_max
+                        index_max = k
+                dptable[0][j][j+i] = Min
+                dptable[1][j][j+i] = Max
+                if index_min == i - 1:
+                    dpstr[0][j][j+i] = dpstr[0][j][j+index_min] + '/' + dpstr[1][j+index_min+1][j+i]
+                else:
+                    dpstr[0][j][j+i] = dpstr[0][j][j+index_min] + '/(' + dpstr[1][j+index_min+1][j+i] + ')'
+                if index_max == i - 1:
+                    dpstr[1][j][j+i] = dpstr[1][j][j+index_max] + '/' + dpstr[0][j+index_max+1][j+i]
+                else:
+                    dpstr[1][j][j+i] = dpstr[1][j][j+index_max] + '/(' + dpstr[0][j+index_max+1][j+i] + ')'
+        return dpstr[1][0][n-1]
