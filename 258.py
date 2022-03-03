a24abeee81592339550f96a9f6203bcdb91e8ca3
muyiyunzi <gt3115a@163.com>
2022-03-03 11:35:59 +0800

    258. 各位相加

--- 258.py
+++ 258.py
@@ -7,3 +7,11 @@ class Solution:
             for i in L:
                 num += int(i)
         return num
+
+# 第二版 数学 O(1)
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num % 9 == 0:
+            return 9 if num !=0 else 0
+        else:
+            return num % 9
