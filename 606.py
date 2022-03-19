39a7dadef58bb06ece85e65a09b80daae0c0c178
muyiyunzi <gt3115a@163.com>
2022-03-19 20:16:00 +0800

    606. 根据二叉树创建字符串

---
+++ 606.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root.left is None and root.right is None:
+            return f"{root.val}"
+        elif root.left is None:
+            return f"{root.val}()({self.tree2str(root.right)})"
+        elif root.right is None:
+            return f"{root.val}({self.tree2str(root.left)})"
+        return f"{root.val}({self.tree2str(root.left)})({self.tree2str(root.right)})"
