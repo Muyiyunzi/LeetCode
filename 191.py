d39d2eefc3289245cbd949021ab15816ad0ad873
muyiyunzi <gt3115a@163.com>
2022-04-05 15:14:01 +0800

    191. 位1的个数

--- 191.py
+++ 191.py
@@ -9,3 +9,11 @@ class Solution:
         return cnt
 
 # n & n-1这种奇技淫巧常数应该更小一些？
+# 好吧也不一定。可能这种大数之间的按位与太耗时了
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        while n: # 每发生一次and操作，去掉一个1，直至全0
+            n &= (n - 1)
+            cnt += 1
+        return cnt
