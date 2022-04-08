b8cd2c8167a428fdc1d6a9df2d2a023d246f85e5
muyiyunzi <gt3115a@163.com>
2022-04-08 15:46:12 +0800

    429. N 叉树的层序遍历

--- 429.py
+++ 429.py
@@ -25,3 +25,22 @@ class Solution:
             ans.append(layer)
             q.extend(nodes)
         return ans[:-1]
+
+# 按照答案的方式，获取length，用for改写了一下，时间上慢不少。
+# 似乎这种边pop边append的方式要慢不少。pop时的长度似乎很影响时间？
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        q = deque([root])
+        ans = []
+        while q:
+            layer = []
+            lq = len(q)
+            for i in range(lq):
+                node = q.popleft()
+                layer.append(node.val) # 注意这里加val，就避免了最后-1的尴尬
+                for i in node.children:
+                    q.append(i)
+            ans.append(layer)
+        return ans
