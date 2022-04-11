f11813fbef73d45618ee7978d7afeb1ce9a7c2e8
muyiyunzi <gt3115a@163.com>
2022-04-11 11:21:54 +0800

    357. 统计各位数字都不同的数字个数

---
+++ 357.py
@@ -0,0 +1,12 @@
+# 此题乘法原理最优解
+# 纯打表
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        ans = [1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851]
+        return ans[n]
+
+# 间接打表，慢4ms
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        ans = [1, 9, 9*9, 9*9*8, 9*9*8*7, 9*9*8*7*6, 9*9*8*7*6*5, 9*9*8*7*6*5*4, 9*9*8*7*6*5*4*3]
+        return sum(ans[:n+1])
