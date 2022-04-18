e5cf3c61cc1d4253a553e59949471f5eedc54d7d
muyiyunzi <gt3115a@163.com>
2022-04-18 14:34:30 +0800

    386. 字典序排数

--- 386.py
+++ 386.py
@@ -1,5 +1,21 @@
+# 用lambda偷懒2333
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         nums = list(range(1, n+1))
         nums.sort(key=lambda x:(str(x)))
         return nums
+
+# DFS
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        q = deque([1,2,3,4,5,6,7,8,9])
+        ans = []
+        while q:
+            now = q.popleft()
+            if now > n:
+                continue
+            else:
+                ans.append(now)
+                now = now * 10
+                q.extendleft(list(range(now+9, now-1, -1)))
+        return ans
