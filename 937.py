807da3b7cfb8a6e0f8959078f1860325c0bfd856
muyiyunzi <gt3115a@163.com>
2022-05-03 12:53:49 +0800

    937. 重新排列日志文件

--- 937.py
+++ 937.py
@@ -2,3 +2,13 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         return sorted(logs, key=lambda x: (x.split(' ')[1][0].isnumeric(), (x.split(' ')[1:], x.split(' ')[0]) if x.split(' ')[1][0].isalpha() else None))
+
+#按照官解，写了一版自定义函数
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def trans(log: str):
+            a, b = log.split(' ', 1)
+            if b[0].isalpha():
+                return (0, b, a)
+            return (1, )
+        return sorted(logs, key=trans)
