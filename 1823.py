9f27abeb31503ccab10ced71fa16de1204712608
muyiyunzi <gt3115a@163.com>
2022-05-04 14:21:12 +0800

    1823. 找出游戏的获胜者

--- 1823.py
+++ 1823.py
@@ -2,3 +2,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         return 1 if n==1 else (self.findTheWinner(n-1, k) + k - 1) % n + 1
+
+# 改成迭代，空间减少但是时间上升
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        winner = 1
+        for i in range(2, n + 1):
+            winner = (winner + k - 1) % i + 1
+        return winner
