3cb43f3ea64588826c1190460acc35935bce18b9
muyiyunzi <gt3115a@163.com>
2022-05-08 22:03:17 +0800

    6058. 统计打字方案数

---
+++ 6058.py
@@ -0,0 +1,32 @@
+# 注意打的表也可以取余
+class Solution:
+    def countTexts(self, pressedKeys: str) -> int:
+        cnt, ans = 0, 1
+        temp = pressedKeys[0]
+        
+        chart1 = [1, 2, 4]
+        chart2 = [1, 2, 4, 8]
+        for i in range(3, 100000):
+            chart1.append((chart1[i-1] + chart1[i-2] + chart1[i-3])% (10**9 + 7))
+        for i in range(4, 100000):
+            chart2.append((chart2[i-1] + chart2[i-2] + chart2[i-3] + chart2[i-4])% (10**9 + 7))
+        
+        n = len(pressedKeys)
+        
+        def check(n):
+            if temp in "79":
+                res = chart2[cnt-1]
+            else:
+                res = chart1[cnt-1]
+            return res
+        
+        for i, val in enumerate(pressedKeys):
+            if val != temp:
+                ans = ans * check(cnt) % (10**9 + 7)
+                temp = val
+                cnt = 1
+            else:
+                cnt += 1
+                if i == n-1:
+                    ans = ans * check(cnt) % (10**9 + 7)
+        return ans
